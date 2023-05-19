413574ace8086a9ad1d51f6898f2d1fa5951520a
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2023-05-19 11:04:29 +0530

    displaying dynamic data as per category

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:meals/data/dummy_dart.dart';
+import 'package:meals/models/category.dart';
 import 'package:meals/screens/meals.dart';
 import 'package:meals/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
-  void _selectCategory(BuildContext context) {
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals = dummyMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
     Navigator.push(
         context,
         MaterialPageRoute(
-            builder: (ctx) => MealsScreen(title: 'sOME TI', meals: [])));
+            builder: (ctx) =>
+                MealsScreen(title: category.title, meals: filteredMeals)));
   }
 
   @override
@@ -31,7 +36,7 @@ class CategoriesScreen extends StatelessWidget {
             CategoryGridItem(
               category: category,
               onSelectCategory: () {
-                _selectCategory(context);
+                _selectCategory(context, category);
               },
             )
         ],
